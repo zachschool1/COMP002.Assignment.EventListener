0480bb5c8f6452cd5352b83bce3be6274a847f20
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-11-13 19:02:50 -0800

    probably a better way to do this but im workingon it

--- script.js
+++ script.js
@@ -7,6 +7,8 @@
 // balloon, without scrolling the page.
 
 let balloon = document.getElementById("balloon");
+
+
 function growBalloon(event) {
 //i had to use google to figure out why it would change pixels to 0, it had to do with my previous code being a string so i had to get do the window.getComptuedStyle (which i knew nothing about) and also use parseFloat (also knew nothing about) and then i had an issue with nothing happening, but it turns out it has to be made back to a string hence the concatenation. very hard especially since I didnt have any resources aside from google, and I feel like googling stuff is kind of cheating, since i think it should be all available through the lecture material, but i didnt have access to linked in learning so it had to be done
     if (event.key === "ArrowUp") {
@@ -18,7 +20,8 @@ function growBalloon(event) {
         balloon.style.fontSize = newSize + "px";
         event.preventDefault();
     }
-
+    //console.log(startingSizeNum); test to see if it still says starting size or not for pop function
+    console.log(popLimit);
 }
 //after you do it first time its easy the 2nd time
 function shrinkBalloon(event) {
@@ -42,6 +45,15 @@ document.addEventListener("keydown", shrinkBalloon);
 // Hint: keeping track of the size in percentage might be easier.
 // Hint: Make sure you quote the emoji characters. They are strings, after all.
 // Hint: document.getElementById("balloon") will get the balloon element on the page.
+let startingSize = window.getComputedStyle(balloon).fontSize
+let startingSizeNum = parseFloat(startingSize);
+
+let popLimit = startingSizeNum * 5;
+console.log(startingSizeNum);
+// function popBalloon(event) {
+//     if 
+// }
+
 
 // 2. The index.html page has a tabbed layout. Make the default state of the layout show
 // the first tab, and make it so that when you click the links at the top the correct
